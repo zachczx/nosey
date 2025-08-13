d80dca4fa734599e5d81ff01643098f98e6ccc60
zachczx <zixian.chen@gmail.com>
2025-08-14 00:39:58 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { pb } from '$lib/pb';
+
+	if (pb.authStore.isValid) {
+		goto('/app');
+	} else {
+		goto('/login');
+	}
+</script>

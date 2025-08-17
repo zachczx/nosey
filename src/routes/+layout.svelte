271d245cd71ca657a94877d08703cfbda83ad2ae
zachczx <zixian.chen@gmail.com>
2025-08-17 22:34:01 +0800

    Added favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { pb } from '$lib/pb';
-
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children?.()}
